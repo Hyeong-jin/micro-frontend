fe6cc7910421b0d23c5026c96de497b338b9d5f5
Hyeong-jin <gilseonappa@gmail.com>
2025-02-23 13:45:44 +0900

    refactor App component to implement custom plugin and enhance module loading functionality

--- packages/mf-consumer/src/App.tsx
+++ packages/mf-consumer/src/App.tsx
@@ -1,32 +1,47 @@
-import { init, loadRemote } from "@module-federation/runtime";
+import { init, loadRemote, registerRemotes } from "@module-federation/runtime";
 import "./App.css";
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
+const CustomPlugin = {
+  name: "custom-plugin",
+  beforeInit(args: any) {
+    console.log("beforeInit: ", args);
+    return args;
+  },
+  init(args: any) {
+    console.log("init: ", args);
+    return args;
+  },
+  beforeRequest(args: any) {
+    console.log("beforeRequest: ", args);
+    return args;
+  },
+  afterResolve(args: any) {
+    console.log("afterResolve: ", args);
+    return args;
+  },
+  onLoad(args: any) {
+    console.log("onLoad: ", args);
+    return args;
+  },
+  async loadShare(args: any) {
+    console.log("loadShare: ", args);
+    return args;
+  },
+  async beforeLoadShare(args: any) {
+    console.log("beforeLoadShare: ", args);
+    return args;
+  },
+};
 
 const options = {
   name: "mf_consumer",
   remotes: [
     { name: "mf_module1", entry: "http://localhost:3001/mf-manifest.json" },
     { name: "mf_module2", entry: "http://localhost:3002/mf-manifest.json" },
-    { name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" },
+    // { name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" },
   ],
-  plugins: [
-    {
-      name: "custom-plugin",
-      beforeInit(args: any) {
-        console.log("beforeInit: ", args);
-        return args;
-      },
-      init(args: any) {
-        console.log("init: ", args);
-        return args;
-      },
-      beforeLoadShare(args: any) {
-        console.log("beforeLoadShare: ", args);
-        return args;
-      },
-    },
-  ],
+  plugins: [CustomPlugin],
   shared: {
     react: {
       version: "18.3.1",
@@ -51,7 +66,7 @@ const options = {
 
 init(options);
 
-const System = ({ moduleId }: { moduleId: string }) => {
+const ModuleLoader = ({ moduleId }: { moduleId: string }) => {
   if (!moduleId) {
     return <h2>No System specified</h2>;
   }
@@ -65,18 +80,29 @@ const System = ({ moduleId }: { moduleId: string }) => {
 
 function App() {
   const [moduleId, setModuleId] = React.useState<string>("");
+
   const loadModule1 = () => setModuleId("mf_module1/app");
   const loadModule2 = () => setModuleId("mf_module2/app");
+
+  const registerModule3 = () => () => {
+    registerRemotes([{ name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" }]);
+  };
   const loadModule3 = () => setModuleId("mf_module3/app");
+
   return (
     <>
       <header>
-        <button onClick={loadModule1}>Load Module 1</button>
-        <button onClick={loadModule2}>Load Module 2</button>
-        <button onClick={loadModule3}>Load Module 3</button>
+        <h1>Module Federation Consumer</h1>
+        <h2>Click on the buttons to load the modules</h2>
+        <button onClick={registerModule3()}>Add Module 3</button>
+        <hr />
+        <button onClick={loadModule1}>Module 1</button>
+        <button onClick={loadModule2}>Module 2</button>
+        <button onClick={loadModule3}>Module 3</button>
+        <hr />
       </header>
       <main>
-        <System moduleId={moduleId} />
+        <ModuleLoader moduleId={moduleId} />
       </main>
     </>
   );
