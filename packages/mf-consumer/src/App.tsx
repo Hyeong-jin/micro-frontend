b419f9e4821324bb0a1ea6b1dfec05d9ddb61d81
Hyeong-jin <gilseonappa@gmail.com>
2025-02-26 23:12:28 +0900

    add launch configuration and package-lock, update App component with error handling and retry logic feat: add new files and update existing ones
    - new file: .vscode/launch.json
    - new file: package-lock.json
    - new file: packages/mf-consumer/package-lock.json
    - modified: packages/mf-consumer/package.json
    - modified: packages/mf-consumer/src/App.tsx
    - new file: packages/mf-module1/package-lock.json
    - new file: packages/mf-module2/package-lock.json
    - new file: packages/mf-module3/package-lock.json
    - modified: pnpm-lock.yaml
    
    Changes include adding new package-lock.json files for various packages, updating the package.json and App.tsx in mf-consumer, and adding a new launch configuration for VSCode.

--- packages/mf-consumer/src/App.tsx
+++ packages/mf-consumer/src/App.tsx
@@ -1,8 +1,12 @@
-import { init, loadRemote, registerRemotes } from "@module-federation/runtime";
-import "./App.css";
-import React, { lazy, Suspense } from "react";
+import { RetryPlugin as LoadModuleRetryPlugin } from "@module-federation/retry-plugin";
+import { FederationRuntimePlugin, init, loadRemote } from "@module-federation/runtime";
+import { Remote } from "@module-federation/runtime/types";
+import React, { lazy, Suspense, useCallback, useEffect } from "react";
 import ReactDOM from "react-dom";
-const CustomPlugin = {
+import { ErrorBoundary } from "react-error-boundary";
+import "./App.css";
+
+const CustomPlugin: FederationRuntimePlugin = {
   name: "custom-plugin",
   beforeInit(args: any) {
     console.log("beforeInit: ", args);
@@ -32,16 +36,46 @@ const CustomPlugin = {
     console.log("beforeLoadShare: ", args);
     return args;
   },
+  errorLoadRemote(args: any) {
+    console.log("[ Module Federation Load Remote Error ]", args);
+    return {
+      default: () => (
+        <div>
+          <h1>Failed to load remote</h1>
+        </div>
+      ),
+    };
+  },
+  loadEntryError(args: any) {
+    console.log("loadEntryError: ", args);
+    return args;
+  },
 };
+
+const initialRemotes = [
+  { name: "mf_module1", entry: "http://localhost:3001/mf-manifest.json" },
+  { name: "mf_module2", entry: "http://localhost:3002/mf-manifest.json" },
+  // { name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" },
+];
 
 const options = {
   name: "mf_consumer",
-  remotes: [
-    { name: "mf_module1", entry: "http://localhost:3001/mf-manifest.json" },
-    { name: "mf_module2", entry: "http://localhost:3002/mf-manifest.json" },
-    // { name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" },
+  remotes: [...initialRemotes],
+  plugins: [
+    CustomPlugin,
+    LoadModuleRetryPlugin({
+      fetch: {},
+      script: {
+        //   retryTimes: 3,
+        //   retryDelay: 100,
+        cb: (resolve, error) => {
+          return setTimeout(() => {
+            resolve(error);
+          }, 100);
+        },
+      },
+    }),
   ],
-  plugins: [CustomPlugin],
   shared: {
     react: {
       version: "18.3.1",
@@ -64,41 +98,83 @@ const options = {
   },
 };
 
-init(options);
+const federationHost = init(options);
 
 const ModuleLoader = ({ moduleId }: { moduleId: string }) => {
   if (!moduleId) {
     return <h2>No System specified</h2>;
   }
+
   const Component = lazy(() => loadRemote<any>(moduleId));
+
   return (
-    <Suspense fallback={<h1>Loading... {moduleId}</h1>}>
-      <Component />
-    </Suspense>
+    <ErrorBoundary fallback={<h1>Failed to load {moduleId}</h1>}>
+      <Suspense fallback={<h1>Loading... {moduleId}</h1>}>
+        <Component />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
 function App() {
+  const [remotes, setRemotes] = React.useState<Remote[]>([...initialRemotes]);
   const [moduleId, setModuleId] = React.useState<string>("");
+
+  useEffect(() => {
+    federationHost.options.remotes = remotes;
+  }, [remotes]);
+
+  const hasRemote = useCallback(
+    (name: string) => {
+      return remotes.some((remote) => remote.name === name);
+    },
+    [remotes],
+  );
 
   const loadModule1 = () => setModuleId("mf_module1/app");
   const loadModule2 = () => setModuleId("mf_module2/app");
 
-  const registerModule3 = () => () => {
-    registerRemotes([{ name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" }]);
+  const registerRemote3 = (name: string) => {
+    if (!hasRemote(name)) {
+      setRemotes([...remotes, { name: "mf_module3", entry: "http://localhost:3003/mf-manifest.json" }]);
+    }
   };
+
+  const removeRemote3 = (name: string) => {
+    setRemotes(remotes.filter((remote) => remote.name !== name));
+  };
+
   const loadModule3 = () => setModuleId("mf_module3/app");
+  const loadModule4 = () => setModuleId("mf_module1/retry-when-network-error");
 
   return (
     <>
       <header>
         <h1>Module Federation Consumer</h1>
-        <h2>Click on the buttons to load the modules</h2>
-        <button onClick={registerModule3()}>Add Module 3</button>
+        <button
+          onClick={() => {
+            registerRemote3("mf_module3");
+          }}
+        >
+          Add Remote 3
+        </button>
+        <button
+          onClick={() => {
+            removeRemote3("mf_module3");
+          }}
+        >
+          Remove Remote 3
+        </button>
         <hr />
         <button onClick={loadModule1}>Module 1</button>
         <button onClick={loadModule2}>Module 2</button>
-        <button onClick={loadModule3}>Module 3</button>
+        {hasRemote("mf_module3") && (
+          <>
+            <button onClick={loadModule3}>Module 3</button>
+          </>
+        )}
+            <button onClick={loadModule4}>Retry fetch</button>
+
         <hr />
       </header>
       <main>
